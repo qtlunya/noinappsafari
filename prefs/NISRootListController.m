ee21963d2c618c1c95d220455995ac121c221be6
nyuszika7h <nyuszika7h@gmail.com>
2021-10-30 18:09:57 +0200

    Add prefs folder

---
+++ prefs/NISRootListController.m
@@ -0,0 +1,13 @@
+#import "NISRootListController.h"
+
+@implementation NISRootListController
+
+- (NSArray *)specifiers {
+    if (!_specifiers) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+    }
+
+    return _specifiers;
+}
+
+@end
